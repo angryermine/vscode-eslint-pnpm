2317ed494ccccc8a8735ae6695a475c913e48933
Leonid Fedorov <angryermine@yandex.ru>
2025-11-26 18:29:02 +0300

    add example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-// Error: Failed to load plugin 'jsx-a11y' declared in '.eslintrc » eslint-config-airbnb » 
+// 1:1   error  Prefer default export on a file with single export  import/prefer-default-export
+// 1:25  error  Newline required at end of file but not found       eol-last
+export function foo() {}
